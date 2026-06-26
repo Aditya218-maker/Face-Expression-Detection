892fc0732ec49048c891d673a966f6a5595d736d
Aditya218-maker <aditya777anand@gmail.com>
2026-06-26 21:58:02 +0530

    refactor: Rename connectDB to connectToDB and improve logging in database connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
-const app = require("./src/app");
-const connectDB = require("./src/config/database");
+require("dotenv").config();
 
-connectDB();
+const app = require("./src/app")
+const connectToDB = require("./src/config/database");
 
-app.listen(3000, ()=>{
-    console.log("FINE: Server is running on port 3000");
+
+connectToDB();
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 })
--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-function connectDB(){
+function connectToDB() {
     mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("CONNECTED TO DB");
-    })
-    .catch(err=>{
-        console.log("ERROR CONNECTING TO DB")
-    })
+        .then(() => {
+            console.log("Connected to DB")
+        })
+        .catch(err => {
+            console.log("Error connecting to DB", err)
+        })
 }
+
+module.exports = connectToDB;
